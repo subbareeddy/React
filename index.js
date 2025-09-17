0ee7d003f172aaaa39331e2e6b5f90afaa273519
Kesari Venkata Subba Reddy <2311CS030452@mallareddyuniversity.ac.in>
2025-09-17 17:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ import App from './App';
 import Button from './Button';
 import ReactStyles from './ReactStyles';
 import Rendering from './Rendering';
+import HooksUseStatePart1 from './HooksUseStatePart1';
+import HooksUseStatePart2 from './HooksUseStatePart2';
+import HooksUseStateExercise01 from './HooksUseStateExercise01';
+import HooksUseStateExercise02 from './HooksUseStateExercise02';
+import HooksUseStateMiniProject from './HooksUseStateMiniProject';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('rootElement'));
 root.render(
   <React.StrictMode>
   <App />
-    <Button />
-    <ReactStyles />
-    <Rendering />
+  <Button />
+  <ReactStyles />
+  <Rendering />
+  <HooksUseStatePart1 />
+  <HooksUseStatePart2 />
+  <HooksUseStateExercise01 />
+  <HooksUseStateExercise02 />
+  <HooksUseStateMiniProject />
   </React.StrictMode>
 );
 
